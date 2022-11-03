7279278b04268bd6446037019b074e5c9505bdff
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-11-03 01:29:01 -0700

    updated app.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+
+import React, { useEffect, useState } from "react";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/index.js
@@ -0,0 +1,11 @@
+function About() {
+    return (
+<div className="container about">
+    
+
+
+</div>
+
+
+    )
+}
