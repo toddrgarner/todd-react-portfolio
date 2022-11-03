6f8bd7c1a5200eb7176c9a27f9bc99eaa69dede6
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-11-03 02:04:20 -0700

    updated index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import React, { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,82 @@
+import { Link } from "react-scroll";
+
 function Nav() {
-  return <nav>
-    nav
-  </nav>;
+  return (
+    <div className="nav">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="container-fluid">
+            <h2>Todd Garner</h2>
+            <button
+              class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around"
+              type="button"
+              data-bs-toggle="collapse"
+              aria-controls="navbarNav"
+              data-bs-target="#navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="toggler-icon top-bar"></span>
+              <span class="toggler-icon middle-bar"></span>
+              <span class="toggler-icon bottom-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <Link
+                    class="category"
+                    to="about"
+                    offset={-100}
+                    duration={500}
+                  >
+                    <a href="about" class="nav-link active">
+                      About
+                    </a>
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link
+                    class="category"
+                    to="skills"
+                    offset={-100}
+                    duration={500}
+                  >
+                    <a href="skills" class="nav-link active">
+                      Skills
+                    </a>
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link
+                    class="category"
+                    to="experience"
+                    offset={-100}
+                    duration={500}
+                  >
+                    <a href="experience" class="nav-link active">
+                      Experience
+                    </a>
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link
+                    class="category"
+                    to="portfolio"
+                    offset={-100}
+                    duration={500}
+                  >
+                    <a href="portfolio" class="nav-link active">
+                      Portfolio
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
