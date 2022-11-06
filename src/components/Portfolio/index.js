3ba5d70ae2e29d089ee01fee7b1d460581ff8054
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-11-06 01:52:03 -0700

    updated portfolio folder

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,15 @@
 import Event from "../../assets/Event-Fan-Follow.webp";
-import PP from "PP-Website.webp";
-import Resume from "Resume-Creator.webp";
-import ToDoList from "To-Do-List-App.webp";
-import ToddPort from "Todd-Portfolio.webp";
+import NoteTaker from "../../assets/Note-Taker.webp";
+import Resume from "../../assets/Resume-Creator.webp";
+import ToDoList from "../../assets/To-Do-List-App.webp";
+import ToddPort from "../../assets/Todd-Garner-Portfolio.webp";
+
+
+
+
 
 function Portfolio() {
     return (
-
         <div className="portfolio">
         <h1>My Portfolio</h1>
         <div className="container">
@@ -20,6 +23,15 @@ function Portfolio() {
                         <a
                         target="blank"
                         rel="noopener noreferrer"
+                        href="https://github.com/AASports89/Event-Fan-Follow"
+                        >
+                            Code
+                        </a>
+                    </button>
+                    <button className="site">
+                        <a
+                        target className="_blank"
+                        rel="noopener noreferrer"
                         href="https://aasports89.github.io/Event-Fan-Follow"
                         >
                             Site
@@ -27,7 +39,123 @@ function Portfolio() {
                     </button>
                 </div>
             </div>
+             <div className="card">
+                <img src={NoteTaker} alt="Note Taker"></img>
+                <h2>
+                    <strong>Note Taker</strong>
+                </h2>
+                <div className="buttons">
+                    <button className="code">
+                        <a
+                        target="blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/toddrgarner/note-taker"
+                        >
+                            Code
+                        </a>
+                    </button>
+                    <button className="site">
+                        <a
+                        target className="_blank"
+                        rel="noopener noreferrer"
+                        href="https://safe-beach-36562.herokuapp.com/"
+                        >
+                            Site
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className="card">
+                <img src={Resume} alt="Resume Creator"></img>
+                <h2>
+                    <strong>Resume Creator</strong>
+                </h2>
+                <div className="buttons">
+                    <button className="code">
+                        <a
+                        target="blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/cindyung56/resume-creator"
+                        >
+                            Code
+                        </a>
+                    </button>
+                    <button className="site">
+                        <a
+                        target className="_blank"
+                        rel="noopener noreferrer"
+                        href="https://fierce-ravine-29010.herokuapp.com"
+                        >
+                            Site
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className="card">
+                <img src={ToDoList} alt="To Do Lis App"></img>
+                <h2>
+                    <strong>To Do List App</strong>
+                </h2>
+                <div className="buttons">
+                    <button className="code">
+                        <a
+                        target="blank"
+                        rel="noopener noreferrer"
+                        href=""
+                        >
+                            Code
+                        </a>
+                    </button>
+                    <button className="site">
+                        <a
+                        target className="_blank"
+                        rel="noopener noreferrer"
+                        href="https://serene-falls-35131.herokuapp.com/"
+                        >
+                            Site
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className="card">
+                <img src={ToddPort} alt="Todd Garner Portfolio"></img>
+                <h2>
+                    <strong>Todd Garner Portfolio</strong>
+                </h2>
+                <div className="buttons">
+                    <button className="code">
+                        <a
+                        target="blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/toddrgarner/todd-garner-portfolio"
+                        >
+                            Code
+                        </a>
+                    </button>
+                    <button className="site">
+                        <a
+                        target className="_blank"
+                        rel="noopener noreferrer"
+                        href="https://toddrgarner.github.io/todd-garner-portfolio"
+                        >
+                            Site
+                        </a>
+                    </button>
+                </div>
+            </div>
+            
+
+           
+            
+
          </div>
-      </div>
+    
+            
+
+         </div>
+      
+      
     )
 }
+
+export default Portfolio;
